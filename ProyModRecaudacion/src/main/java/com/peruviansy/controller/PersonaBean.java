e85785a1979fb5af6cd7e71e7cd7ff21b36d6245
henryvargasguevara <henryvargasguevara@gmail.com>
2018-04-30 21:16:09 -0500

    segundo commit

---
+++ ProyModRecaudacion/src/main/java/com/peruviansy/controller/PersonaBean.java
@@ -0,0 +1,327 @@
+package com.peruviansy.controller;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.ServletContext;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+import com.peruviansy.model.Persona;
+import com.peruviansy.service.IPersonaService;
+import com.peruviansy.service.Impl.PersonaServiceImpl;
+
+@Named
+@ViewScoped
+public class PersonaBean implements Serializable{
+	
+	private List<Persona> lstPersonas=new ArrayList<>();
+	private String url=new String();
+	private String url2=new String();
+	
+	private String nombre;
+	private String apellido;
+	private double monto;
+	private int nro;
+	private File file2;
+	private Date fechainicio;
+	private Date fechafinal;
+	private LocalDate fechainicio1;
+	private LocalDate fechafinal1;
+	
+	@Inject
+	private Persona persona;
+	
+	@Inject
+	private IPersonaService service;
+	private UploadedFile file;
+
+	
+	@PostConstruct
+	public void init() 
+	{     nombre=new String();
+	      this.apellido=new String();
+	    	this.listar();
+	}
+	  
+    public void listar()
+    {
+    	try {
+    		lstPersonas=service.listar();
+    		
+    	}catch(Exception e) {
+    		
+    	}
+    }
+    
+    public void listarxId() {
+    	try 
+    	{
+    		//lstPersonas=service.ListarPorId(this.persona);
+    	}catch(Exception e) {
+    		
+    	}
+    }
+
+    
+    
+    
+	public String getUrl2() {
+		return url2;
+	}
+
+	public void setUrl2(String url2) {
+		this.url2 = url2;
+	}
+
+	public Date getFechainicio() {
+		return fechainicio;
+	}
+
+	public void setFechainicio(Date fechainicio) {
+		this.fechainicio = fechainicio;
+	}
+
+	public Date getFechafinal() {
+		return fechafinal;
+	}
+
+	public void setFechafinal(Date fechafinal) {
+		this.fechafinal = fechafinal;
+	}
+
+	public double getMonto() {
+		return monto;
+	}
+
+	public void setMonto(double monto) {
+		this.monto = monto;
+	}
+
+	public int getNro() {
+		return nro;
+	}
+
+	public void setNro(int nro) {
+		this.nro = nro;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Persona getPersona() {
+		return persona;
+	}
+
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+
+	public List<Persona> getLstPersonas() {
+		return lstPersonas;
+	}
+
+	public void setLstPersonas(List<Persona> lstPersonas) {
+		this.lstPersonas = lstPersonas;
+	}
+
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public UploadedFile getFile() {
+		return file;
+	}
+	
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+	
+	  public void upload() {
+	        if(file != null) {
+	            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+	            FacesContext.getCurrentInstance().addMessage(null, message);
+	        }
+	        this.url=file.getFileName();
+	    }
+	  
+	  public void myFileUpload() throws IOException {
+		 
+		  File file2 = new File(file.getFileName());  
+	      // System.out.println(file2.getCanonicalPath()); 
+	      //System.out.println(file2.getPath()); 
+	      //System.out.println(file2.getAbsoluteFile()); 
+		  
+		  this.url=this.file.getFileName()  ;
+		  //ServletContext servletContext=(ServletContext)FacesContext.getCurrentInstance().getExternalContext().getContext();
+		 // this.url2=servletContext.getRealPath("")+File.separator+"upload"+File.separator+this.url;
+		  FacesMessage msg=new FacesMessage("Succesful"+this.url+" is Uploaded");
+		  FacesContext.getCurrentInstance().addMessage(null,msg);
+		  //System.out.println(this.url2);
+		  registrar();
+		  
+	  }
+	  
+	  public void myFileUploadMasivo() throws IOException, EncryptedDocumentException, InvalidFormatException 
+	  {
+		  String diaa=""; 
+		  //File file2 = new File(file.getFileName());  
+		  String urll=file.getFileName();
+		  
+		  boolean band=true;
+		  int mes=Integer.parseInt(urll.substring(19,21));
+		  int anio=Integer.parseInt(urll.substring(22,24));
+		  int dia=1;
+		while(dia<31 && band) {  
+		 
+		  if(dia<10) 
+		  {
+		     diaa="0"+String.valueOf(dia);
+		  }
+		  else 
+		  {
+			   diaa=String.valueOf(dia);
+		  }
+		  String fecha=diaa+"-"+urll.substring(19,21)+"-"+urll.substring(22,24)+".xls";
+		  int mess=Integer.parseInt(urll.substring(19,21));
+		  
+		  System.out.println(urll.substring(19,21));//mes
+		  System.out.println(urll.substring(22,24));//anio
+		  
+		  File archivoExcel = new File("D:/carga/"+"Sistemas_recaud."+fecha);
+		  //abrir el archivo con POI
+		  if(archivoExcel.exists()) 
+		  {
+			 /* Workbook workbook = WorkbookFactory.create(archivoExcel);
+			  HSSFSheet sheet = (HSSFSheet) workbook.getSheetAt(0);
+			  Row row = (  sheet).getRow(0);
+			  String valor=row.getCell(0).toString();
+			  String valor1=row.getCell(1).toString();
+			  //System.out.println(valor);
+			  //System.out.println(valor1);*/
+			  this.url2="Sistemas_recaud."+fecha;
+			  System.out.println("D:/carga/"+"Sistemas_recaud."+fecha);
+			  FacesMessage msg=new FacesMessage("Archivo cargado "+this.url2);
+			  FacesContext.getCurrentInstance().addMessage(null,msg);
+			  //System.out.println(this.url2);
+			  registrar();	
+		  }
+		  
+		  
+		   if(mess==1||mess==3||mess==5||mess==7||mess==8||mess==10||mess==12)
+		   {   if(dia<=31)
+		         dia++;
+		       else
+		    	 band=false;
+		   }
+		   else if(mess==2){
+			      if(dia<=28) {
+			         dia++;
+			      }else {
+			    	  band=false;
+			           }
+		        }else
+		        {
+		        	if(dia<=30)
+		        	{
+		        	   dia++;
+		        	}
+		        	else 
+		        	 {
+		        		band=false;
+		        	}
+		          }
+	      } 
+		
+	  }
+	  
+
+	public void registrar() 
+	{
+		try {
+			
+		//Calendar cal=Calendar.getInstance();
+		//cal.setTime(fechaSeleccionada);
+		
+		//LocalDate localDate=LocalDate.of(cal.get(Calendar.YEAR),cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+		
+		Persona per=new Persona();
+		per.setNombre("Henry Vargas");
+		service.registrar(per,this.url2);
+		
+	    }catch(Exception e) 
+		{
+		e.printStackTrace();
+	    }
+
+   }
+	
+	public void listarxPersona() {
+		try {
+		  this.monto=0;
+		  Persona per=new Persona();
+	     
+		  per.setNombre(this.nombre);
+		  per.setDependencia(this.apellido);
+		  
+		  Calendar cal= Calendar.getInstance();
+		  cal.setTime(fechainicio);
+		  fechainicio1=LocalDate.of(cal.get(Calendar.YEAR),cal.get(Calendar.MONTH)+1,cal.get(Calendar.DAY_OF_MONTH));
+		  
+		  Calendar cal1=Calendar.getInstance(); 
+		  cal1.setTime(fechafinal);
+		  fechafinal1=LocalDate.of(cal1.get(Calendar.YEAR),cal1.get(Calendar.MONTH)+1,cal1.get(Calendar.DAY_OF_MONTH));
+		  
+		  //List<Persona> lst=new ArrayList<>();
+		  //this.lstPersonas=lst;
+		  this.lstPersonas= service.listarxPersona(per,fechainicio1,this.fechafinal1);
+		  for(Persona p : lstPersonas) {
+			  monto=monto+p.getImporte();
+		  }
+		  this.nro=lstPersonas.size();
+		  
+		  System.out.println(cal1.get(Calendar.MONTH)+"****"+cal.get(Calendar.MONTH));
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
---
+++ ProyModRecaudacion/src/main/java/com/peruviansy/dao/impl/PersonaDAOImpl.java
@@ -0,0 +1,291 @@
+package com.peruviansy.dao.impl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
+
+import com.peruviansy.dao.IPersonaDAO;
+import com.peruviansy.model.Persona;
+
+//@Named
+//@RequestScoped
+@Stateless
+public class PersonaDAOImpl implements IPersonaDAO,Serializable {
+	
+	//private EntityManagerFactory emf;
+	@PersistenceContext(unitName="PersonalPU")
+	private EntityManager em;
+	private List<Persona> lstPersonas;
+	private int cont;
+	
+	
+
+	public PersonaDAOImpl() {
+		//emf=Persistence.createEntityManagerFactory("PersonalPU");
+		//em=emf.createEntityManager();
+		lstPersonas=new ArrayList<>();
+		cont=0;
+		
+	}
+	
+
+	public void registrar(Persona per,String url)  throws Exception{
+		// TODO Auto-generated method stub
+		System.out.println("[CDI]Se regisro : "+per.getNombre());
+		this.mostrarExcel(url);
+		try {
+			
+			for(Persona per3 :lstPersonas)
+			  {
+			//em.getTransaction().begin();
+	           if(cont==0) {
+			      em.persist(per3);//PARA INSERT.....MERGE ES PARA ACTUALIZAR
+			      //em.getTransaction().commit();
+	           }else {
+				   em.merge(per3);
+			    }
+	           cont++;
+		
+			 }
+			
+		}catch(PersistenceException e) {
+			/*if(em.getTransaction().isActive()) 
+			{  
+				em.getTransaction().rollback();
+			}*/
+			System.out.println("PersonaDAOImpl aquiiiii");
+			System.out.println(e.getMessage()+"    "+e.getCause());
+			System.out.println(e.getLocalizedMessage());
+			
+		}finally {
+		
+		}
+	}
+
+	public void modificar(Persona per) throws Exception {
+		// TODO Auto-generated method stub
+		em.merge(per);
+		
+	}
+
+	public List<Persona> listar() throws Exception 
+	{
+		List<Persona> lista=new ArrayList<>();
+		Query q=em.createQuery("FROM Persona p order by fecha asc");
+		lista=(List<Persona>) q.getResultList();
+		return lista;
+	}
+	
+	public void mostrarExcel(String urll) throws IOException, EncryptedDocumentException, InvalidFormatException {
+		Date fechaSeleccionada;
+		//paso 0. Definir una colección con nombres de las columnas a procesar
+		//considera que esto lo puedes leer de un archivo de configuración,
+		//input de usuario o cualquier otra fuente
+		List<String> columnas = Arrays.asList("MONEDA","DEPENDENCIA","CONCEP","NUMERO","CODIGO","NOMBRE","IMPORTE","FECHA");
+		//paso 1.
+		Map<String,Integer> mapNombresColumnas = new TreeMap<String,Integer>();
+		//paso 2.
+		//número de fila donde están los nombres de celda
+		//recuerda que POI está basado con índice 0
+		//si tus nombres están en la fila 1, entonces deberías iniciar esta
+		//variable con 0.
+		final int filaNombresColumnas =0;
+		//url representa el nombre del archivo excel a subir
+		//String url2=urll.toString();
+		//System.out.println(url2.substring(16,18));
+		//System.out.println(url2.substring(19,21));
+		//System.out.println(url2.substring(22,24));
+		
+		File archivoExcel = new File("D:/carga/"+urll);
+		//abrir el archivo con POI
+		Workbook workbook = WorkbookFactory.create(archivoExcel);
+		//ubicarse en la hoja donde vas a procesar
+		//si es la primera hoja, debes indicar 0
+		HSSFSheet sheet = (HSSFSheet) workbook.getSheetAt(0);
+		//acceder a la fila con los nombres de las columnas
+		Row row = (  sheet).getRow(filaNombresColumnas);
+		//paso 3.
+		//utilizando el poder de Java 8
+		row.cellIterator().forEachRemaining(cell -> {
+		    //paso 3.1.
+		    String valorCelda = cell.getStringCellValue().trim();
+		    
+		    //Persona per=new Persona();
+		    //per.setMoneda(cell.getStringCellValue().trim());
+		    //per.setDependencia(cell.getStringCellValue().trim());
+		    if (!valorCelda.isEmpty()) 
+		    {
+		        mapNombresColumnas.put(valorCelda, cell.getColumnIndex());
+		    }
+		});
+		//paso 4.
+		//se asume que los valores para procesar se encuentran en la fila
+		//siguiente a la fila donde están los nombres de las columnas
+		int indiceDatos = filaNombresColumnas + 1;
+		Row filaDatos = null;
+		List<Persona> lstPersonas1=new ArrayList<>();
+		//recorrer todas las filas con datos
+		while ((filaDatos = ((org.apache.poi.ss.usermodel.Sheet) sheet).getRow(indiceDatos++)) != null) {
+		    //se procesan solo las celdas en base a los "nombres" de esas columnas
+		       //el resultado de mapNombresColumnas.get(col) es
+		       //el número de columna a leer
+		       //en este caso, solo se imprime el resultado
+		       //puedes reemplazar esto por la manera en que debas procesar la información
+			 
+			   Persona pers=new Persona();
+			   LocalDate ff;
+			 if(!((filaDatos.getCell(mapNombresColumnas.get("MONEDA"))).toString()).equalsIgnoreCase("")) {  
+			   pers.setUrl(urll);
+			   String monedaa=(((filaDatos.getCell(mapNombresColumnas.get("MONEDA"))).toString()) );
+			  // System.out.println("***"+monedaa);
+			   pers.setMoneda(monedaa.substring(0,3));
+			   //System.out.println(monedaa.substring(0,3));
+			   pers.setDependencia(filaDatos.getCell(mapNombresColumnas.get("DEPENDENCIA"))+"");
+			   String concep=(filaDatos.getCell(mapNombresColumnas.get("CONCEP"))+"");
+			   //System.out.println(concep);
+			   pers.setConcepto(concep.substring(0,6)); 
+			   String nume=filaDatos.getCell(mapNombresColumnas.get("NUMERO"))+"";
+               String numeroo="";
+			   
+			   if(nume.substring(1,2).equals(".") && nume.length()==3) {
+				   
+			   }
+			   else if(numeroo.length()==9)
+                      {   
+				       numeroo=nume.substring(0,7);
+				      }
+				   else  if(nume.length()==11)
+                         {   
+				           numeroo=nume.substring(0,1)+nume.substring(2,9);
+				         }
+				         else if(nume.length()==10) 
+				              {
+				        	    numeroo=nume.substring(0,1)+nume.substring(2,8);
+				              }
+				              else if(nume.length()==7) 
+				                   {
+				            	    numeroo=nume.substring(0,5);
+				                   }
+				                  else if(nume.length()==8) {
+				                	  numeroo=nume.substring(0,6);
+				                  }
+			   pers.setNumero(numeroo);
+			   String codi=filaDatos.getCell(mapNombresColumnas.get("CODIGO"))+"";
+			   String codigoo="";
+			   
+			   if(codi.substring(1,2).equals(".")&&codi.length()==3) {
+			
+				  // pers.setCodigo();
+			   }
+			   else if(codi.length()==9)
+                      {   
+				       codigoo=codi.substring(0,7); 
+				      }
+				   else  if(codi.length()==11)
+                         {   
+				           codigoo=codi.substring(0,1)+codi.substring(2,9);
+				         }
+				         else if(codi.length()==10) 
+				              {
+				        	   codigoo=codi.substring(0,1)+codi.substring(2,8);
+				              }
+				              else if(codi.length()==7) 
+				                   {
+				            	    codigoo=codi.substring(0,5);
+				                   }
+				                  else if(codi.length()==8) {
+				                	  codigoo=codi.substring(0,6);
+				                  }
+
+			   pers.setCodigo(codigoo);
+			   
+			   pers.setNombre(filaDatos.getCell(mapNombresColumnas.get("NOMBRE"))+"");
+			   pers.setImporte(  Double.parseDouble((filaDatos.getCell(mapNombresColumnas.get("IMPORTE")).toString())));
+			   String fechaa=urll;
+			   
+               ff=LocalDate.of(Integer.parseInt("20"+fechaa.substring(22,24)),Integer.parseInt(fechaa.substring(19,21)),Integer.parseInt((fechaa.substring(16,18))));
+			  
+			   pers.setFecha(ff);
+		       //System.out.println(pers.getId()+"AAA/"+pers.getMoneda()+"/"+pers.getDependencia()+"/"+pers.getConcepto()+"/"+pers.getNumero()+
+		    		//   "/"+pers.getCodigo()+"/"+pers.getNombre());	    
+			   lstPersonas1.add(pers);
+		    }
+		  }	 
+		
+		   this.lstPersonas=lstPersonas1;
+		}
+
+	
+	@Override
+	public Persona ListarPorId(Persona t) throws Exception {
+		List<Persona> lista=new ArrayList<>();
+		Query q=em.createQuery("FROM Persona p where p.id = ?");
+		q.setParameter(1,t.getId());
+		lista=(List<Persona>) q.getResultList();
+		
+		Persona per=lista != null && !lista.isEmpty() ? lista.get(0) : new Persona();
+	
+		return per;
+	  }
+
+
+	@Override
+	public List<Persona> listarxPersona(Persona t,LocalDate inicio,LocalDate fin) throws Exception 
+	 { /* There are two approaches to parameter binding: using positional or using
+		named parameters. Hibernate and Java Persistence support both options, but you
+		can’t use both at the same time for a particular query.
+		With named parameters, you can rewrite the query as
+		String queryString =
+		"from Item item where item.description like :search";
+		*/
+		
+		List<Persona> lista =new ArrayList<>();
+		Query q=em.createQuery("From Persona p where (p.nombre LIKE  :code) AND ( p.nombre LIKE :code1)"
+				+ " AND (p.fecha BETWEEN :startDate AND :endDate)");
+		//Query q=em.createQuery("From Persona p where p.id = 363 ");
+		q.setParameter("code","%"+t.getNombre()+"%");
+		q.setParameter("code1","%"+t.getDependencia()+"%");
+		q.setParameter("startDate",inicio);
+		q.setParameter("endDate",fin);
+		
+		System.out.println("'%"+t.getNombre()+"%'");
+		lista=(List<Persona>) q.getResultList();
+		//Persona p2=lista.get(0);
+		//System.out.println(p2.getNombre()+" / "+p2.getDependencia());
+		return lista;
+	  }
+	
+	}
+
+	
+
